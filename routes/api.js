4c5d9e063e35b6b1bbe15dbe7f8e5fc606468f95
arnav-t <avznav@gmail.com>
2018-12-05 17:31:09 +0530

    Add delete method

--- routes/api.js
+++ routes/api.js
@@ -8,14 +8,36 @@ router.get('/', (req, res) => {
 	res.send({success: true});
 });
 
+// Add a cab
 router.post('/add', (req, res) => {
 	Cab.create(req.body).then(cab => {
 		res.send({ 
 			success: true,
 			data: cab 
 		});
+	},
+	reason => {
+		res.status(422).send({
+			success: false,
+			error: reason.message
+		});
 	});
 });
+
+// Delete a cab
+router.delete('/delete/:id', (req,res) => {
+	Cab.findByIdAndRemove({_id: req.params.id}). then(cab => {
+		if(cab !== null)
+			res.send({
+				success: true,
+				data: cab
+			});
+		else
+			res.status(404).send({
+				success: false
+			});
+	});
+})
 
 // Export router
 module.exports = router;
